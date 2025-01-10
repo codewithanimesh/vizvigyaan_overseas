ac91cb5079e15685981f54edd995d6b21645ccfe
shubham321-ux <shubham4260shubhu@gmail.com>
2025-01-10 18:34:57 +0530

    upadte in footer

--- src/Componenets/Footer.js
+++ src/Componenets/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../Componenets/css/footer.css";
+import "../Componenets/css/Footer.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Footer = () => {
